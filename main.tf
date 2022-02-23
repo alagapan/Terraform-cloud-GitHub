6a6868d5548832614edda7fcf0ce6477816a9be8
alagapan <alagapan@yahoo.com>
2022-02-24 00:05:25 +0530

    updated corrections as needed to run

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.1" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.1"
      }
     }
    }
 
 provider "aws" {
-       # region = "us-east-1" 
+        region = "us-east-1" 
         profile = "default"
-	}
+}
 
 resource "aws_instance" "Raminfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
